fb0700a8abe6499a605821ebdabefdbf05900aaa
Fuxx-1 <583742849@qq.com>
2021-08-28 21:31:01 +0800

    2021-08-28

--- sign_in.py
+++ sign_in.py
@@ -27,7 +27,7 @@ def action():
     print("此次打卡人数：" + str(len(username)))
     for i in range(0, len(username)):
         res = SignIn(i, 0)  # 请求
-        ReInf(res, i)
+        # ReInf(res, i)
     print(getTimeStr() + " 打卡完毕")
 
 
