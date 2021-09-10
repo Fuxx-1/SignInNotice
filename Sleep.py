c3fc79f8359644984de55a0a20ed943efe7af99b
Fuxx-1 <583742849@qq.com>
2021-09-10 18:57:49 +0800

    2021-09-10

--- Sleep.py
+++ Sleep.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     while True:
         time_now = datetime.datetime.utcnow() + datetime.timedelta(hours=8.0)  # 获取时间
         print(time_now.hour)
-        if time_now.hour == 7 or time_now.hour == 12 or time_now.hour == 18:
+        if time_now.hour == 7 or time_now.hour == 12:
             break
         else:
             # 时间未到，等待至下一小时的第30分钟，(^ v ^)
